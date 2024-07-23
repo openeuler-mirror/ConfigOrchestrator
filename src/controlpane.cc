bf65671648cd15571658ca0cea8389e5c24cdbe1
Li Yiyan <lyy0627@sjtu.edu.cn>
2024-07-23 20:18:35 +0800

    refactor: enable debugging first

--- src/controlpane.cc
+++ src/controlpane.cc
@@ -2,14 +2,15 @@
 #include "backend/config_manager.h"
 #include "frontend/main_menu.h"
 #include "frontend/ui_base.h"
+#include "tools/cplog.h"
 
 #include <memory>
 
 auto main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) -> int {
-  YUI::app()->setApplicationTitle("Control Panel");
-
   YUILog::setLogFileName("/tmp/controlpanel.log");
   YUILog::enableDebugLogging();
+
+  YUI::app()->setApplicationTitle("Control Panel");
 
   auto configManager = std::make_shared<ConfigManager>();
   auto menu = std::make_shared<MainMenu>("Main Menu", configManager);
