8b87e7cd025e8bcb3801bf242eaca022b336b353
Li Yiyan <lyy0627@sjtu.edu.cn>
2024-07-24 19:40:56 +0800

    refatcor: change test dirs

--- test/backend/package_manager/package_manager_test.cc
+++ test/backend/package_manager/package_manager_test.cc
@@ -1,18 +1,18 @@
 #include <gtest/gtest.h>
 #include <memory>
 
-#include "backend/package_manager_backend.h"
+#include "backend/package_manager/package_manager_backend.h"
 
 class PackageManagerTest : public ::testing::Test {
 protected:
-  void SetUp() override { fwb = std::make_shared<PackageManagerBackend>(); }
+  void SetUp() override { pkb = std::make_shared<PackageManagerBackend>(); }
 
   void TearDown() override {}
 
-  std::shared_ptr<PackageManagerBackend> fwb;
+  std::shared_ptr<PackageManagerBackend> pkb;
 };
 
-TEST_F(PackageManagerTest, get_chain) {}
+TEST_F(PackageManagerTest, get_packages) { ASSERT_EQ(0, 0); }
 
 auto main(int argc, char **argv) -> int {
   ::testing::InitGoogleTest(&argc, argv);
